6a5acb6d399443819f665b3a4f539620b26ddfc5
AnilOsmanTur <aaotrr@gmail.com>
2016-04-14 23:03:22 +0300

    klasör işlemleri genişletildi. Artık change direction dediğimizde otomatik olarak gerekli klasörler açılıyor. Ek olarak bazı terimlerin ingilizceleştirilmesi yapıldı gerekliliği konusunda yorum istiyorum. scrol bar eklendi sadece duygu listesinin bitmediğini göstermek için.

--- StudentFaceRecording.py
+++ StudentFaceRecording.py
@@ -1,16 +1,18 @@
 import Tkinter as tk
-from Tkinter import *
-import cv2
-from PIL import Image, ImageTk
-import tkMessageBox
 import os
+import cv2
+from Tkinter import *
+from PIL import Image, ImageTk
+from tkMessageBox import *
 
 
-#Global degiskenlerin olusturulma alani
+
+#Global variables
 width = 800
 height = 600
-itemsforlistbox=['Gulme','Kizma','Uzulme','Igrenme','Korkma','Normal']
-dictionaryForCheckEmotions = {"Gulme":False, "Kizma":False, "Uzulme": False, "Igrenme": False, "Korkma": False, "Normal":False}
+emotionList=['Angery','Happy','Sad','Fearful','Suprised','Disgusted', 'Normal']
+dictionaryForCheckEmotions = {"Angery":False, "Happy":False, "Sad": False, "Fearful": False, "Suprised": False, "Disgusted":False, "Normal":False}
+selectingEmotion = ""
 
 originFolder = os.getcwd()
 print originFolder
@@ -30,6 +32,34 @@ def UserIDDoneButtonPressed():
     print userID
 
 
+# directory checking and creating 
+def directoryCheckCreate():
+    for directoryName in emotionList:
+        folderName = os.path.join(originFolder, directoryName)
+        if not os.path.isdir(folderName):
+            os.mkdir(folderName)
+       
+def checkPathIsThereIfnCreate():
+    if not os.path.isdir(originFolder):
+        os.makedirs(originFolder)
+        for directoryName in emotionList:
+            folderName = os.path.join(originFolder, directoryName)
+            os.mkdir(folderName)
+    else:
+        directoryCheckCreate()
+    
+        
+def changeDirButtonAction():
+    r = askyesno(title = 'Do you want to change directory path?', message = 'After change needed directories under this directory will be created if there is not any.')
+    if r :
+        global originFolder
+        originFolder = directoryPath.get()
+        checkPathIsThereIfnCreate()
+    else:
+        checkPathIsThereIfnCreate()
+    os.chdir(originFolder)
+    
+    
 #Fotograflari cekilen ogrencinin cekimi bitiginde siradaki ogrenciye gececek ve file ismi olarak siradaki ogrenci yazacak olan yapi
 
 def StudentPictureDone():
@@ -41,7 +71,7 @@ def StudentPictureDone():
             untakenEmotionList = untakenEmotionList + ", " + controlDictionary
 
     if not untakenEmotionList == "":
-        tkMessageBox.showinfo("Uyari", "Lutfen %s' duygu fotograflarini cekiniz!"%untakenEmotionList)
+        showinfo("Warning", "Please,take pictures of %s' emotion(s)!"%untakenEmotionList)
 
     else:
         print untakenEmotionList
@@ -58,51 +88,45 @@ def StudentPictureDone():
 
 # Ogrencinin kacinci fotografini cektigini belirten ve bu fotograflari kaydeden fonksiyon
 def takePicturePressed():
-
+    global selectingEmotion
     if entryUserID.get() == '':
-        tkMessageBox.showinfo("Uyari", "Lutfen UserID' yi bos birakmayiniz!")
+        showinfo("Uyari", "Lutfen UserID' yi bos birakmayiniz!")
     else:
         global personImageNumber
 
         print userID
 
         checkImageIsNone = cv2.imread('%s/%s_%i.png'%(selectingEmotion,userID,personImageNumber))
-
+        print checkImageIsNone
         if checkImageIsNone is None:
 
             cv2.imwrite('%s/%s_%i.png'%(selectingEmotion,userID,personImageNumber),newImage)
 
             personImageNumber += 1
-
+            
             if not dictionaryForCheckEmotions[selectingEmotion]:
             
                 dictionaryForCheckEmotions[selectingEmotion] = True
         else:
-
+	  
             personImageNumber += 1
-
+            
             takePicturePressed()
         
 
 
 def CurSelect(evt):
-
     global selectingEmotion
-
-    selectingEmotion = str((mylistbox.get(mylistbox.curselection())))
-
+    selectingEmotion = str((emotionListBox.get(emotionListBox.curselection())))
+    print selectingEmotion
 
 
 cap = cv2.VideoCapture(0)
-
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 
-
 #Tkinter ile button kurulumu icin gerekli root variable' i
-
 
 
 root = tk.Tk()
@@ -111,28 +135,35 @@ root.minsize(800,620)
 
 root.resizable(width = False, height = False)
 
-mylistbox = Listbox(root, width = 11, height = 4, font = ('times',13))
 
-mylistbox.bind('<<ListboxSelect>>',CurSelect)
+# list box view
+emotionListBox = Listbox(root, width = 11, height = 4, font = ('times',13))
+emotionListBox.bind('<<ListboxSelect>>',CurSelect)
+for items in emotionList:
+    emotionListBox.insert(END,items)
+emotionListBox.place(x = 97, y = 512)
 
-for items in itemsforlistbox:
 
-    mylistbox.insert(END,items)
+scrollbar = Scrollbar(root)
+scrollbar.place(x = 81, y = 512, height = 95)
 
-
+emotionListBox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=emotionListBox.yview)
 
 
 directoryPath = Entry(root, width=60)
 directoryPath.place(x = 80, y = 4)
 directoryPath.insert(END, originFolder)
 
-changeDir = tk.Button(root, text = 'Change Direction', command=None )
+changeDir = tk.Button(root, text = 'Change Direction', command=changeDirButtonAction)
 changeDir.place(x = 586, y = 0)
 
-takeAPictureButton = tk.Button(root, text="Take a Picture", command=takePicturePressed)
+takePictureButton = tk.Button(root, text="Take a Picture", command=takePicturePressed)
+takePictureButton.place(x = 321, y = 514)
+
 
 passAnotherStudentButton = tk.Button(root, text="Student Picture Done", command=StudentPictureDone)
-
+passAnotherStudentButton.place(x = 301,y = 550)
 
 
 labelUserID = Label(root, text = "UserID")
@@ -141,12 +172,8 @@ labelUserID.place(x = 525,y = 538)
 entryUserID = Entry(root, bd = 3)
 entryUserID.place(x = 595, y = 536)
 
-
-
 userIDDoneButton = Button(root, text = "Tamam", command = UserIDDoneButtonPressed)
 userIDDoneButton.place(x = 610, y = 570)
-
-
 
 
 
@@ -155,16 +182,6 @@ root.bind('<Escape>', lambda e: root.quit())
 # images taken by the camera
 lmain = tk.Label(root, width = 640, height = 480)
 lmain.place(x = 80, y = 30)
-
-
-
-#buttonlarin bizim canvasimiza eklenilmesi
-
-mylistbox.place(x = 80, y = 512)
-
-takeAPictureButton.place(x = 321, y = 514)
-
-passAnotherStudentButton.place(x = 301,y = 550)
 
 
 
