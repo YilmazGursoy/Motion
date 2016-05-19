b90f2cdaeec138fac2c974a5b3f9a660d576ca2b
AnilOsmanTur <aaotrr@gmail.com>
2016-05-19 22:40:19 +0300

    saving as a video option added

--- motionDetection.py
+++ motionDetection.py
@@ -9,6 +9,10 @@ def main():
     camera.set(3, 640)
     camera.set(4, 480)
 
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
     initial_frame = cv2.cvtColor(camera.read()[1], cv2.COLOR_BGR2GRAY).astype('float')
     value = 0
     while True:
@@ -32,7 +36,8 @@ def main():
         if cnts:
             print "Hareket kaydedildi!!" , counter
             cv2.waitKey(2)
-            cv2.imwrite("Images/image%04i.jpg" %counter, image)
+            out.write(image)
+            #cv2.imwrite("Images/image%04i.jpg" %counter, image)
             counter += 1
         
         for c in cnts:
@@ -45,6 +50,10 @@ def main():
 
         if cv2.waitKey(1) & 0xFF is ord('q'):
             break
+    # Release everything if job is finished
+    camera.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
