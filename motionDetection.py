0b00430eb41b45e4902a85cffb3c900c9a171f7b
Yilmaz Gursoy <yilmazgursoyy@gmail.com>
2016-05-20 01:13:59 +0300

    Framelerin Video ile alımı
    Framelerin video ile alımı sırasında her hareketsizlik algılanıldığında
    yeni bir video yaratmaması için bu duraksama süresi eger 1 dakikadan az
    ise video devam edilmektedir fazla ise yeni video create edilmektedir

--- motionDetection.py
+++ motionDetection.py
@@ -1,18 +1,33 @@
-# -*- coding: utf-8 -*-
-
+import datetime
+import os
 import cv2
+import time
 import numpy as np
+
+global timerStart
+timerStart = time.time()
+
 
 def main():
     counter = 0
     camera = cv2.VideoCapture(0)
     camera.set(3, 640)
     camera.set(4, 480)
+    
+    newMotion = False
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-
+    dateNow = datetime.datetime.now()
+    year = str( getattr(dateNow, 'year') )
+    month = str( getattr(dateNow, 'month') )
+    day = str( getattr(dateNow, 'day') )
+    
+    originFolder = os.getcwd()
+    folderName = os.path.join(originFolder, year, month, day)
+    if not os.path.isdir(folderName):
+        os.makedirs(folderName)
+        
     initial_frame = cv2.cvtColor(camera.read()[1], cv2.COLOR_BGR2GRAY).astype('float')
     value = 0
     while True:
@@ -34,21 +49,42 @@ def main():
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
         if cnts:
+            if counter is 0:
+                newMotion = True
+            
+            if newMotion:
+                dateNow = datetime.datetime.now()
+                hour = str( getattr(dateNow, 'hour') )
+                minute = str( getattr(dateNow, 'minute') )
+                second = str( getattr(dateNow, 'second') )
+                name = folderName +'/'+ hour +'-'+ minute +'-'+ second
+                out = cv2.VideoWriter("%s.avi" %name,fourcc, 20.0, (640,480))
+                newMotion = False
             print "Hareket kaydedildi!!" , counter
-            cv2.waitKey(2)
             out.write(image)
-            #cv2.imwrite("Images/image%04i.jpg" %counter, image)
             counter += 1
+            global timerStart
+            timerStart = time.time()
+        
+        else:
+            global timerEnd
+            timerEnd = time.time()
+            hours, rem = divmod(timerEnd - timerStart, 3600)
+            minutes, seconds = divmod(rem, 60)
+            print "Seconds = ",seconds
+            if minutes == 1:
+                print "Yeni Resme Gecildi"
+                newMotion = True 
+
         
         for c in cnts:
             x, y, w, h = cv2.boundingRect(c)
-
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         cv2.imshow("Webcam", image)
         cv2.imshow("Thresh", thresh)
 
-        if cv2.waitKey(1) & 0xFF is ord('q'):
+        if cv2.waitKey(1) & 0xFF is ord('q') or cv2.waitKey(1) & 0xFF is 27:
             break
     # Release everything if job is finished
     camera.release()
