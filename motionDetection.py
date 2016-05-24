b3e22cde71f8d888a139aa5fe95209c986353080
AnilOsmanTur <aaotrr@gmail.com>
2016-05-24 11:49:42 +0300

    scollbar and zooming added and area threshold added

--- motionDetection.py
+++ motionDetection.py
@@ -1,18 +1,88 @@
 import datetime
-import os
 import cv2
+import os
 import time
 import numpy as np
 
-global timerStart
+
+#Global variables
+MAX_CAM_RESOLUTION_WIDTH = 7680
+MAX_CAM_RESOLUTION_HEIGHT = 4800
+DEFAULT_CAM_RESOLUTION_WIDTH = 0
+DEFAULT_CAM_RESOLUTION_HEIGHT = 0
+
 timerStart = time.time()
 
+frameWidth = 640
+frameHeight = 480
+
+camWidth = 640
+camHeight = 480
+frameArea = frameWidth * frameHeight
+
+changeThres = 0.00
+areaThresh = 0
+zoom = 0
+
+cX = 0
+cY = 0
+start_x = 0
+start_y = 0
+end_x = 0
+end_y = 0
+
+def trackbar_change(x):
+    pass
+
+def zoom_trackbar_change(x):
+    pass
+
+def changeCenterArea():
+    global cX
+    global cY
+    global start_x
+    global start_y
+    global end_x
+    global end_y
+    cX = camWidth / 2 
+    cY = camHeight / 2
+    start_y = cY - (frameHeight / 2)
+    end_y = cY + (frameHeight / 2)
+    start_x = cX - (frameWidth / 2)
+    end_x = cX + (frameWidth / 2)
+
+ 
+def changedCamRatio(camera):
+    global camWidth
+    global camHeight
+    global frameArea
+    camWidth = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+    camHeight = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        
+    
 
 def main():
     counter = 0
+    global MAX_CAM_RESOLUTION_WIDTH
+    global MAX_CAM_RESOLUTION_HEIGHT
+    global DEFAULT_CAM_RESOLUTION_WIDTH
+    global DEFAULT_CAM_RESOLUTION_HEIGHT
+    #capture object creation
     camera = cv2.VideoCapture(0)
-    camera.set(3, 640)
-    camera.set(4, 480)
+    DEFAULT_CAM_RESOLUTION_WIDTH = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+    DEFAULT_CAM_RESOLUTION_HEIGTH = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, MAX_CAM_RESOLUTION_WIDTH)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, MAX_CAM_RESOLUTION_HEIGHT)
+
+    MAX_CAM_RESOLUTION_WIDTH = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+    MAX_CAM_RESOLUTION_HEIGTH = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    MAX_ZOOM = MAX_CAM_RESOLUTION_WIDTH / frameWidth
+
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
+    changedCamRatio(camera)
     
     newMotion = False
 
@@ -22,19 +92,37 @@ def main():
     year = str( getattr(dateNow, 'year') )
     month = str( getattr(dateNow, 'month') )
     day = str( getattr(dateNow, 'day') )
-    
+
+    #directory creation
     originFolder = os.getcwd()
     folderName = os.path.join(originFolder, year, month, day)
     if not os.path.isdir(folderName):
         os.makedirs(folderName)
-        
-    initial_frame = cv2.cvtColor(camera.read()[1], cv2.COLOR_BGR2GRAY).astype('float')
-    value = 0
-    while True:
-        ret, image = camera.read()
 
+    # Create a window
+    cv2.namedWindow('Camera')
+
+    # create trackbars for color change
+    cv2.createTrackbar('Threshold','Camera',0,10,trackbar_change)
+    cv2.createTrackbar('Zoom','Camera', 0, int(MAX_ZOOM), zoom_trackbar_change)
+    track = 0
+      
+      
+    changeCenterArea()
+    first_img = camera.read()[1]
+    first_img = first_img[start_y:end_y, start_x:end_x]
+    initial_frame = cv2.cvtColor(first_img, cv2.COLOR_BGR2GRAY).astype('float')    
+    
+    while(1):
+        ret, image = camera.read() 
         if not ret:
             break
+        
+        #finding the center and cropping       
+        changeCenterArea()
+        image = image[start_y:end_y, start_x:end_x]
+
+        ##-------
 
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(image_gray, (21, 21), 0)
@@ -47,8 +135,16 @@ def main():
 
         _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    
+        changedArea = 0;
+        for c in cnts:
+            x, y, w, h = cv2.boundingRect(c)
+            changedArea += w * h
 
-        if cnts:
+        changeRatio = float(changedArea) / float(frameArea)
+        print changeRatio
+         
+        if changeRatio >= changeThres:
             if counter is 0:
                 newMotion = True
             
@@ -75,22 +171,48 @@ def main():
             if minutes == 1:
                 print "Yeni Resme Gecildi"
                 newMotion = True 
-
         
         for c in cnts:
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+             
 
-        cv2.imshow("Webcam", image)
+        cv2.imshow('Camera',image)
         cv2.imshow("Thresh", thresh)
-
-        if cv2.waitKey(1) & 0xFF is ord('q') or cv2.waitKey(1) & 0xFF is 27:
+        
+        key = cv2.waitKey(1)
+        if key & 0xFF is ord('q') or key & 0xFF is 27:
             break
-    # Release everything if job is finished
+
+        # get current positions of four trackbars
+        global changeThres
+        global areaThresh
+        if cv2.getTrackbarPos('Threshold','Camera') != areaThresh :
+	    areaThresh = cv2.getTrackbarPos('Threshold','Camera')
+            changeThres = float(areaThresh) / 100.0
+        global zoom
+        if cv2.getTrackbarPos('Zoom','Camera') != track :
+            track = cv2.getTrackbarPos('Zoom','Camera')
+            zoom = track
+            if zoom == 0:
+	        zoom = 1
+	    elif zoom == 1:
+	        zoom = 1.5
+            camera.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth * zoom)
+            camera.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight * zoom)
+            #after changing the zoom we need to change the initial frame
+            changedCamRatio(camera)
+            changeCenterArea()
+            first_img = camera.read()[1]
+            first_img = first_img[start_y:end_y, start_x:end_x]
+            initial_frame = cv2.cvtColor(first_img, cv2.COLOR_BGR2GRAY).astype('float')
+        print zoom
+        print changeThres
+
+
     camera.release()
     out.release()
     cv2.destroyAllWindows()
-
-
+    
 if __name__ == '__main__':
     main()
